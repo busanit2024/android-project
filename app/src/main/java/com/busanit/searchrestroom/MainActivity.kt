56085d386435c64529c0ea02790b9d0afef7dae8
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-11-05 16:47:52 +0900

    프로젝트 파일 생성

--- app/src/main/java/com/busanit/searchrestroom/MainActivity.kt
+++ app/src/main/java/com/busanit/searchrestroom/MainActivity.kt
@@ -1,37 +1,20 @@
 package com.busanit.searchrestroom
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.busanit.searchrestroom.databinding.ActivityMainBinding
-import com.busanit.searchrestroom.MyPageActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // BottomNavigationView 설정
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.activity_home -> {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    true
-                }
-                R.id.my_favorite -> {
-                    startActivity(Intent(this, FavoriteActivity::class.java))
-                    true
-                }
-                R.id.activity_mypage -> {
-                    // 마이페이지로 이동
-                    startActivity(Intent(this, MyPageActivity::class.java))
-                    true
-                }
-                else -> false
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_main)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
     }
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,22 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("androidx.*")
+      }
     }
-    plugins {
-        id("com.android.application") version "8.1.4"
-        id("org.jetbrains.kotlin.android") version "1.9.0"
-    }
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "SearchRestroom"

fd6f65adb42b465fa9acc8a8f2d132cd7d100356
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-11-06 17:42:55 +0900

    메인페이지 레이아웃 구성

--- app/src/main/java/com/busanit/searchrestroom/entity/Restroom.kt
+++ app/src/main/java/com/busanit/searchrestroom/entity/Restroom.kt
@@ -11,13 +11,15 @@ data class Restroom (
   val restroomId: Int,
   @ColumnInfo(name = "restroom_name")
   val restroomName: String,
-  val location: String,
+  val location: String, // 한글 주소
+  val latitude: Double, // 위도
+  val longitude: Double, // 경도
   @ColumnInfo(name = "open_time")
-  val openTime: String,
+  val openTime: String, // 개방 시간
   @ColumnInfo(name = "full_time")
-  val fullTime: Boolean,
-  val unisex: Boolean,
-  val diaper: Boolean,
-  val accessible: Boolean,
+  val fullTime: Boolean, // 24시간 개방 여부
+  val unisex: Boolean, // 남녀공용 여부
+  val diaper: Boolean, // 기저귀 교환대 유무
+  val accessible: Boolean, // 장애인 화장실 유무
   val memo: String
 )

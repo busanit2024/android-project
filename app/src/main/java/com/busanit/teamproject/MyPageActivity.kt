798d3a1bb23dad0ffb9e6286febedb8df43edee1
Leeeeee <dltkdgjs002@naver.com>
2024-11-07 12:05:35 +0900

    화면 및 즐겨찾기, 정보수정 페이지 intent 완료

--- app/src/main/java/com/busanit/teamproject/MyPageActivity.kt
+++ app/src/main/java/com/busanit/teamproject/MyPageActivity.kt
@@ -12,9 +12,26 @@ class MyPageActivity : AppCompatActivity() {
         val binding = ActivityMypageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.myReview.setOnClickListener {
+            // 나의 리뷰
+        }
+
+        binding.myRestroom.setOnClickListener {
+            // 등록한 화장실
+        }
+
+        binding.myFavorite.setOnClickListener {
+            // 즐겨찾기
+            val intent = Intent(this, FavoriteActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.adminPage.setOnClickListener {
+            // 관리자 페이지(단, 로그인 했을 때만 보이게 하기!)
+        }
+
         binding.logout.setOnClickListener {
             // 로그아웃
-
         }
 
         binding.editInfo.setOnClickListener {

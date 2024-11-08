b6a2f48bed5ec81316c3c81c449befc0b941ca8c
Leeeeee <dltkdgjs002@naver.com>
2024-11-08 09:58:13 +0900

    화면 및 즐겨찾기, 정보수정 페이지 intent 완료

---
+++ app/src/main/java/com/busanit/teamproject/dao/RestroomDao.kt
@@ -0,0 +1,23 @@
+package com.busanit.teamproject.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.busanit.teamproject.database.Restroom
+
+@Dao
+interface RestroomDao {
+    @Query("select * from restroom")
+    fun getAll(): List<Restroom>
+
+    @Query("select * from restroom where restroom_id = :id")
+    fun getRestroomById(id: Int): Restroom
+
+    @Insert
+    fun insert(vararg restroom: Restroom)
+
+    @Delete
+    fun delete(restroom: Restroom)
+
+}

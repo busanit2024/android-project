f0973831f2329eea157b4baf705258915f6efbdf
dduhell <suhyun97a@gmail.com>
2024-11-13 15:10:08 +0900

    로그인, 회원가입 뒤로가기 버튼 및 아이디 찾기 레이아웃 생성

---
+++ app/src/main/java/com/busanit/searchrestroom/member/FindIdActivity.kt
@@ -0,0 +1,14 @@
+package com.busanit.searchrestroom.member
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FindIdActivity : AppCompatActivity() {
+
+    private lateinit var binding:
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/busanit/searchrestroom/member/FindPwActivity.kt
@@ -0,0 +1,4 @@
+package com.busanit.searchrestroom.member
+
+class FindPwActivity {
+}

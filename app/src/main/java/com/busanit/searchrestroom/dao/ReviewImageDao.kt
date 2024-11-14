90d8036f3475ad557fc8c0bcda66a439408ae71b
Leeeeee <dltkdgjs002@naver.com>
2024-11-14 12:55:20 +0900

    - MyReviewActivity에 해당 memberId로 적은 것과 그것에 해당하는 reviewId의 글이 뜨게끔 수정 (리뷰 등록할 때 reviewDB에는 안 들어가는 것 같습니다) - 샘플 데이터 삭제 - 데이터는 각각의 Dao에 연결 - ReviewImageDao의 getReviewImageById 함수를 List로 선언했습니다
    + 기타 오타 수정

--- app/src/main/java/com/busanit/searchrestroom/dao/ReviewImageDao.kt
+++ app/src/main/java/com/busanit/searchrestroom/dao/ReviewImageDao.kt
@@ -14,7 +14,7 @@ interface ReviewImageDao {
   @Query("select * from review_image where review_id = :id")
   fun getReviewImageById(id: Int): List<ReviewImage>
   // List<ReviewImage>로 바뀌었습니다!
-  // - 리뷰 이미지를 하나만 한다면 몰라도 여러 개 넣을 수도 있어서 List를 걸었습니다.
+  // - 리뷰 이미지를 하나만 한다면 몰라도 여러 개 넣을 수도 있어서 List 걸었습니다.
 
 
   @Query("select * from review_image where image_id = :id")

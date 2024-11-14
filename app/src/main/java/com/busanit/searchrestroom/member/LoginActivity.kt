b1bb563fd6e35a39b3a220231141e2a422cabe40
dduhell <suhyun97a@gmail.com>
2024-11-14 14:57:27 +0900

    로그인 시 공란 두지 않도록 설정

--- app/src/main/java/com/busanit/searchrestroom/member/LoginActivity.kt
+++ app/src/main/java/com/busanit/searchrestroom/member/LoginActivity.kt
@@ -43,6 +43,12 @@ class LoginActivity : AppCompatActivity(){
             val email = binding.loginEmail.text.toString()
             val password = binding.loginPassword.text.toString()
 
+            // 입력란 공란 체크
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "모든 정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             // 로그인 시도
             viewModel.loginUser(email, password)
         }
